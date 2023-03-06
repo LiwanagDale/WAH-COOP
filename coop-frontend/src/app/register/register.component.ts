8524508122625489a050f3f9b783f5e9e33ed212
Alipioallen <111748932+Alipioallen@users.noreply.github.com>
2023-03-06 20:48:14 +0800

    Sign up
    Register UI

---
+++ coop-frontend/src/app/register/register.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit, OnDestroy, ViewEncapsulation, Inject } from '@angular/core';
+
+import { DOCUMENT } from '@angular/common';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css'],
+  encapsulation: ViewEncapsulation.None,
+})
+export class RegisterComponent implements OnInit, OnDestroy  {
+
+  constructor(@Inject(DOCUMENT) private _document: any){}
+
+  ngOnInit() {
+    this._document.body.classList.add('body');
+  }
+
+  ngOnDestroy() {
+    this._document.body.classList.add('body');
+  }
+
+}
